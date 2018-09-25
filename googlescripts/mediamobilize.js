2c14b6393e2c651c1c3614ab11bf30ae42339d5c
Jeff Lee <jlee@flipboard.com>
2018-09-25 12:04:34 -0700

    yay it sorta works

--- googlescripts/mediamobilize.js
+++ googlescripts/mediamobilize.js
@@ -1,9 +1,23 @@
 function onOpen() {
-  var sheet = SpreadsheetApp.getActive();
-  var menubuttons = [
-    {name:"Generate Ad", functionName:"parseUgly"}
-  ];
-  sheet.addMenu("Generate", menubuttons)
+  SpreadsheetApp.getUi()
+  .createMenu('Generate')
+  .addItem("Generate Ad", 'openDialog')
+  .addToUi();
+}
+
+function doGet() {
+  return HtmlService
+  .createHtmlOutputFromFile('preview')
+  .evaluate();
+}
+
+function openDialog() {
+  var html = HtmlService
+  .createTemplateFromFile('preview')
+  .evaluate();
+  SpreadsheetApp
+  .getUi()
+  .showModalDialog(html, 'Output');
 }
 
 function link_(link, text) {
@@ -34,10 +48,15 @@ function promoted_(data) {
   ];
 }
 
+function clean(text) {
+  return text.replace('\n', '').trim();
+}
+
 function headline_(data) {
+  var text = clean(data['text']);
   return [
     {
-    'text':link_(data['link'], data['text']),
+    'text':link_(data['link'], text),
     'style':'font-size:27px;line-height:33px;font-family:Georgia, Times, serif;font-weight:bold;',
     'line_class':'article_title font21'
     }
@@ -53,7 +72,7 @@ function image_(data) {
       'width':566,
       'image_class':'imageScale',
       'web_url':data['link'],
-      'space':'10',
+      'space':'16',
       'space_class':'h10',
       'align':'center'
     }
@@ -62,23 +81,46 @@ function image_(data) {
 
 function body_(data) {
   var body = data['text'];
-  var stylediv = 'color:#070707;text-align:left;line-height:26px;Margin-bottom:15px;';
-  var stylea = 'text-decoration: none; font-size:17px; color:#000000;';
-  var class = 'font14 paddingbottom20';
+  var stylediv = 'font-size:17px;font-family: Georgia, Helvetica, Arial, sans-serif;text-align:left;line-height:26px;Margin-bottom:15px;';
+  var stylea = 'text-decoration:none;color:#000000;';
+  var styleDomain = 'font-family: Helvetica, Arial, sans-serif; font-weight:bold;';
+  var cssClass = 'font14 paddingbottom20';
   var domain = data['domain'] || '';
-  var tmpl = '<div style="__stylediv__" class="__class__">' + 
+  var tmpl = '<div class="__class__">' + 
     '<a class="article_text_link" href="{{smartlink(web_url=\'__link__\', **utm)}}" style="__stylea__">' +
-      '<span class="article_author" style="font-family: Helvetica, Arial, sans-serif; font-weight:bold;">__domain__</span> ' +
-        '<span class="article_text" style="font-family: Georgia, Helvetica, Arial, sans-serif;">__body__</a>';
-  //'web_url':data['link'],
+      '<span class="article_author" style="__styleDomain__">__domain__</span> ' +
+        '<span class="article_text">__body__</a>';
+
+  var output = tmpl
+  .replace('__body__', clean(data['text']))
+  .replace('__class__', cssClass)
+  .replace('__link__', data['link'])
+  .replace('__stylea__', stylea)
+  .replace('__styleDomain__', styleDomain)
+  .replace('__domain__', data['domain']);
   return [
     {
-      'text':data['text'],
+      'text':output,
       'space':'10',
       'space_class':'h10',
-      'style':''
+      'style':stylediv,
+      'line_class':cssClass
     }
   ];
+}
+
+function convertToConfig(data, index) {
+  var template = "body.{index}.{key} = {value}\n";
+  var output = "";
+  for (var key in data) {
+    var value = data[key];
+    output += template
+    .replace('{index}', (index+1)*10)
+    .replace('{key}', key)
+    .replace('{value}', value);
+  }
+  output += "\n";
+  return output;
 }
 
 function parseUgly() {
@@ -86,19 +128,22 @@ function parseUgly() {
   var selection = sheet.getSelection();
   var range = selection.getActiveRange();
   var data = range.getValues();
-  var output = "";
   var mapper = {};
   for (var i=0; i<data.length; i++) {
     var row = data[i];
     var key = row[0].toLowerCase().replace(':','').replace(' ','');
-    var val = row[1];
+    var val = clean(row[1]);
     mapper[key] = val;
   }
   var promoted = promoted_({'text':data[0][0]});
   var headline = headline_({'link':mapper['link'], 'text':mapper['headline']});
   var image = image_({'link':mapper['link'], 'image':mapper['image'], 'text':data[0][0]});
-  var body = body_({'link':mapper['link'], 'text':mapper['body']});
+  var body = body_({'link':mapper['link'], 'text':mapper['body'], 'domain':mapper['domain']});
   var module = promoted.concat(headline, image, body);
-  Logger.log(JSON.stringify(module));
-  return module;
+  var final = "body.default_padding = 42\n" + 
+    "body.default_padding_class = w10" +
+      "\n\n";
+  final += module.map(convertToConfig).join('\n');
+  Logger.log(final);
+  return final;
 }
